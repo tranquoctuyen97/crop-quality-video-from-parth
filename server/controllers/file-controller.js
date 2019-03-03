20b8898048598ee030054cf60e1081f7c6386786
tranquoctuyen97 <tuyentq.1997@gmail.com>
2019-03-04 00:13:34 +0700

    crop video from file parth

--- server/controllers/file-controller.js
+++ server/controllers/file-controller.js
@@ -3,26 +3,22 @@
 import { response, fileHelper } from '../helpers';
 import fs from 'fs';
 
-
 export default class FileController {
 
     cropVideo = async (req, res, next) => {
        try {
-           const { filePath, quality = 480, ouput } = req.body;
-           if(!Number.isInteger(quality)) {
-               return response.returnError(res, new Error('quality is invalid !'));
-           }
+           const { filePath, ouput } = req.body;
            if (!filePath) {
               return response.returnError(res, new Error('filePath is not exist !'));
            }
            if (!fs.existsSync(filePath)) {
                return response.returnError(res, new Error('File is not exist !'));
            }
-           const isCropVideo = fileHelper.cropVideo(filePath, quality, ouput);
+           const isCropVideo = await fileHelper.cropVideo(filePath);
            if(!isCropVideo) {
-               return response.returnError(res, new Error('Error Crop Video !'))
-           }
-           return response.returnSuccess(res, 'Crop video done !' );
+               return response.returnError(res, new Error('Error Crop Video !'));
+            }
+           return response.returnSuccess(res, 'Crop video done !');
 
        } catch (error) {
            return response.returnError(res, error);
--- server/helpers/file-helper.js
+++ server/helpers/file-helper.js
@@ -4,45 +4,53 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 
 export default class FileHelper {
 
-    async cropVideo (filePath, quality, output) {
-        try {
-            ffmpeg.setFfmpegPath(ffmpegPath);
-            const proc = ffmpeg(filePath);
-            // set video bitrate
-            proc.videoBitrate(quality)
-            // set target codec
-            // .videoCodec('divx')
-            // set aspect ratio
-                .aspect('16:9')
-                // set size in percent
-                .size('50%')
-                // set fps
-                .fps(24)
-                // set audio bitrate
-                .audioBitrate('128k')
-                // set audio codec
-                .audioCodec('libmp3lame')
-                // set number of audio channels
-                .audioChannels(2)
-                // set custom option
-                .addOption('-vtag', 'DIVX')
-                // set output format to force
-                .format('avi')
-                // setup event handlers
-                .on('end', function() {
-                    console.log('file has been converted succesfully');
-                    return true;
-                })
-                .on('error', function(err) {
-                    console.log('an error happened: ' + err.message);
-                    return false;
-                })
-                // save to file
-                .save(output);
-        } catch (error) {
-            
+    async cropVideo (filePath) {
+      try {
+        const videoInfo = await this.getVideoInfor(filePath);
+        console.log(videoInfo)
+        if (videoInfo.size > 720) {
+            await this.genQuality(filePath, videoInfo.name);
+            return true;
         }
+      } catch (error) {
+          return false;
+      }       
     };
+    getVideoInfor (filePath) {
+        ffmpeg.setFfmpegPath(ffmpegPath);
+        return new Promise((resolve, reject) => { 
+            ffmpeg.ffprobe(filePath, function(err, metadata) {
+                if (err) {
+                     reject(err);
+                } else {
+                    resolve({
+                        size: metadata.streams[0].height,
+                        name: metadata.streams[0].width + 'x'+ metadata.streams[0].height + '_' + metadata.format.size +'kb' +'_720.mp4'
+                    });   
+                }
+            });
+
+        });
+    };
+    genQuality (filePath, output) {
+        ffmpeg.setFfmpegPath(ffmpegPath);
+        const proc = ffmpeg(filePath);
+        return new Promise((resolve, reject) => { 
+            const video = proc.videoBitrate(720)
+            .on('end', function() {
+                console.log('file has been converted succesfully');
+                 resolve(true);
+            })
+            .on('error', function(err) {
+                console.log(err);
+                 reject(err);
+            })
+            .save(output);
+            console.log(video)
+
+        });
+    }
+
 
      
 
